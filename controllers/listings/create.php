f29bd15fe219684e1bc8b3e21f629cee44bbe2ad
Matt Ballard <mattjballard81@gmail.com>
2024-06-15 23:44:43 +0800

    Set up basic router with controller files

---
+++ controllers/listings/create.php
@@ -0,0 +1,3 @@
+<?php
+
+echo 'Create a New Listing';
--- helpers.php
+++ helpers.php
@@ -44,3 +44,29 @@ function loadPartial($name)
     echo "Partial {$name} not found.";
   }
 }
+
+/**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value)
+{
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+/**
+ * Inspect a value and kill script
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+  echo '<pre>';
+  die(var_dump($value));
+  echo '</pre>';
+}
